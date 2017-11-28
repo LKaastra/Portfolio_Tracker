aa240435b6c746cf622eac1d5794a066ec99542d
lkaastra <lucakaas@gmail.com>
2017-11-28 14:59:03 -0500

    Working on add coin

--- app/src/main/java/com/lucak/portfolio_tracker/Home.java
+++ app/src/main/java/com/lucak/portfolio_tracker/Home.java
@@ -3,12 +3,14 @@ package com.lucak.portfolio_tracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lucak.Database.db;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener{
     private Button addToken;
     private Button addCoin;
 
@@ -19,7 +21,20 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Intent i = getIntent();
-
+        addToken.setOnClickListener(this);
+        addCoin.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnAddCoin:
+
+                break;
+            case R.id.btnAddToken:
+                
+            default:
+                Toast.makeText(this, "Error Occurred", Toast.LENGTH_LONG).show();
+        }
+    }
 }
