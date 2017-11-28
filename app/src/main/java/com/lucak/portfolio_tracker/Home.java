0332d7f6997a5caee75ddc1ac3791f6559dd2b73
lkaastra6885 <lucakaas@gmail.com>
2017-11-28 11:32:25 -0500

    Database done. Working on creating DAL

--- app/src/main/java/com/lucak/portfolio_tracker/Home.java
+++ app/src/main/java/com/lucak/portfolio_tracker/Home.java
@@ -3,12 +3,16 @@ package com.lucak.portfolio_tracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.lucak.Database.db;
 
 public class Home extends AppCompatActivity {
-    private TextView greeting;
+    private Button addToken;
+    private Button addCoin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
